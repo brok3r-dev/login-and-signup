373838ed7662cc99f080c94f861034e5b01c9b00
Jaehun Kang <brok3r@naver.com>
2021-12-21 15:09:29 +0900

    2nd commit: User Entity Update
    1. extends UserDetails

--- src/main/kotlin/com/jay/login/entity/User.kt
+++ src/main/kotlin/com/jay/login/entity/User.kt
@@ -5,6 +5,8 @@ import com.jay.login.common.idAndNameValidation
 import com.jay.login.common.passwordValidation
 import com.jay.login.model.request.UserRequest
 import lombok.NoArgsConstructor
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
 import java.io.Serializable
 import java.sql.Date
 import java.time.LocalDate
@@ -22,7 +24,7 @@ data class User(
     var id: String? = null,
 
     @Column(name = "password", nullable = false)
-    var password: String? = null,
+    var userPassword: String? = null,
 
     @Column(name = "name", nullable = false)
     var name: String? = null,
@@ -30,12 +32,27 @@ data class User(
     @Column(name = "email", nullable = true)
     var email: String? = null,
 
+    @Column(name = "authority", nullable = false)
+    var authority: String? = null,
+
+    @Column(name = "non_expired", nullable = false)
+    var nonExpired: Boolean? = true,
+
+    @Column(name = "non_locked", nullable = false)
+    var nonLocked: Boolean? = true,
+
+    @Column(name = "credential_non_expired", nullable = false)
+    var credentialNonExpired: Boolean? = true,
+
+    @Column(name = "enabled", nullable = false)
+    var enabled: Boolean? = true,
+
     @Column(name = "created_at", nullable = false)
     var createdAt: Date? = null,
 
     @Column(name = "modified_at", nullable = false)
     var modifiedAt: Date? = null
-) : Serializable {
+) : Serializable, UserDetails {
     constructor(request: UserRequest) : this() {
         if (idAndNameValidation(request.id)) {
             this.id = request.id
@@ -44,7 +61,7 @@ data class User(
         }
 
         if (passwordValidation(request.password)) {
-            this.password = request.password
+            this.userPassword = request.password
         } else {
             //TODO: validationError
         }
@@ -66,4 +83,20 @@ data class User(
         this.createdAt = Date.valueOf(LocalDate.now())
         this.modifiedAt = Date.valueOf(LocalDate.now())
     }
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPassword(): String = this.userPassword ?: throw Exception("Invalid Password")
+
+    override fun getUsername(): String = this.name ?: throw Exception("Invalid Username")
+
+    override fun isAccountNonExpired(): Boolean = this.nonExpired ?: false
+
+    override fun isAccountNonLocked(): Boolean = this.nonLocked ?: false
+
+    override fun isCredentialsNonExpired(): Boolean = this.credentialNonExpired ?: false
+
+    override fun isEnabled(): Boolean = this.enabled ?: false
 }
