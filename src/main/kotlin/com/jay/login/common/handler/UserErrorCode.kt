c4ad7cda393fab2e4261a9d84a2d9c363ff37da0
unknown <brok3r@naver.com>
2022-01-11 15:03:19 +0900

    9th commit: user service modified

--- src/main/kotlin/com/jay/login/common/handler/UserErrorCode.kt
+++ src/main/kotlin/com/jay/login/common/handler/UserErrorCode.kt
@@ -3,6 +3,7 @@ package com.jay.login.common.handler
 enum class UserErrorCode {
     USER_REGISTER_FAIL,
     USER_ALREADY_EXIST,
+    USER_DOES_NOT_EXIST,
     INVALID_ID_FORMAT,
     INVALID_NAME_FORMAT,
     INVALID_PASSWORD_FORMAT,
--- src/main/kotlin/com/jay/login/service/UserService.kt
+++ src/main/kotlin/com/jay/login/service/UserService.kt
@@ -7,12 +7,14 @@ import com.jay.login.entity.User
 import com.jay.login.model.request.UserRequest
 import com.jay.login.model.response.UserResponse
 import com.jay.login.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
     private val userRepository: UserRepository
-) {
+): UserDetailsService {
     fun register(request: UserRequest): UserResponse {
         if (userRepository.findById(request.id)?.isNotEmpty() == true) {
             val response = UserErrorResponse(UserErrorCode.USER_ALREADY_EXIST)
@@ -21,5 +23,12 @@ class UserService(
 
         val user = userRepository.save(User(request))
         return UserResponse(user)
+    }
+
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByName(username)
+        user?.let {
+            return it[0]
+        } ?: throw UserException(UserErrorResponse(UserErrorCode.USER_DOES_NOT_EXIST))
     }
 }
