b4f55ab9d127d8622c8d8dc97227c76a9665a064
unknown <brok3r@naver.com>
2022-01-17 19:09:50 +0900

    12th commit: mvc configuration modified

--- src/main/kotlin/com/jay/login/common/config/MvcConfig.kt
+++ src/main/kotlin/com/jay/login/common/config/MvcConfig.kt
@@ -1,12 +1,10 @@
 package com.jay.login.common.config
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebMvc
 class MvcConfig : WebMvcConfigurer {
     override fun addViewControllers(registry: ViewControllerRegistry) {
         registry.apply {
