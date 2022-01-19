63eabc25babf96a5372f9b067880443644d154f5
unknown <brok3r@naver.com>
2022-01-19 15:08:18 +0900

    15th commit: user exception handler modified

--- src/main/kotlin/com/jay/login/common/handler/UserExceptionHandler.kt
+++ src/main/kotlin/com/jay/login/common/handler/UserExceptionHandler.kt
@@ -1,17 +1,17 @@
 package com.jay.login.common.handler
 
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ControllerAdvice
 class UserExceptionHandler {
     @ExceptionHandler(UserException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleUserException(ex: UserException) : ResponseEntity<UserErrorResponse> {
-        val response = UserErrorResponse(ex.response.code)
-        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    fun handleUserException(ex: UserException): UserErrorResponse {
+        return UserErrorResponse(ex.response.code)
     }
 }
